fa33d3af07bc717df70ba47f40d613ee1c71f20e
iamandreribeiro <andreribeiroorafael@gmail.com>
2022-08-26 00:28:31 -0300

    função para manter conexão na sala

--- script.js
+++ script.js
@@ -2,10 +2,24 @@ let pessoa = {
     name: prompt("Qual seu lindo nome?")
 };
 
-function perguntaNome() {
+function enviaNome() {
+    // Envia o nick para API dos Participantes
     const envia = axios.post("https://mock-api.driven.com.br/api/v6/uol/participants", pessoa);
 
-    envia.then((resposta) => {
-        console.log(resposta);
-    });
+    //Recebe a resposta da API e chama da função mantemConexão
+    envia.then(mantemConexao);
 }
+
+function mantemConexao() {
+    //A cada 5 segundos, manda uma requisição para a API para manter a conexão
+    setInterval(function() {
+        const mantem = axios.post("https://mock-api.driven.com.br/api/v6/uol/status", pessoa);
+
+        //Recebe a resposta da API e mostra no console
+        mantem.then((curintia) => {
+            console.log(curintia.status);
+        })
+    }, 5000);
+}
+
+enviaNome();
